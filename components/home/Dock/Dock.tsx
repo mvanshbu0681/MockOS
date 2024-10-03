97bd5a5e6f495698b862bdfe1476ecf1c2be3168
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-10-03 22:14:44 +0530

    fixed icons

--- components/home/Dock/Dock.tsx
+++ components/home/Dock/Dock.tsx
@@ -7,8 +7,8 @@ const DockMain = () => {
   return (
     <div>
       <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2">
-        <div className="bg-opacity-70 backdrop-blur-md rounded-2xl p-4 border border-[#ffffff25] dock-container">
-          <div className="flex items-center space-x-4">
+        <div className="bg-opacity-10 backdrop-blur rounded-xl p-4 border border-[#ffffff25] dock-container">
+          {/*<div className="flex items-center space-x-4">*/}
             {/* <a className='hover:scale-110 ease-in-out duration-300' href="https://www.linkedin.com/in/arnabroy25" target="_blank" rel="noopener noreferrer">
               <FaLinkedin size={25} color="white" />
             </a>
@@ -23,7 +23,7 @@ const DockMain = () => {
             </a> */}
             <Dock itemWidth={50} magnification={1} magnifyDirection='up'>
 
-              {['github-logo.png', 'linkedin-logo'].map((letter) => (
+              {['finder','slack', 'spotify', 'guitar-pro', 'terminal', 'trash'].map((letter) => (
                 <img
                   className="letter"
                   src={`${letter}.png`}
@@ -35,7 +35,7 @@ const DockMain = () => {
           </div>
         </div>
       </div>
-    </div>
+    // </div>
   )
 }
 
